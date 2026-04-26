c28560189f74e345b985640724a82fffcacb070b
Kajal <kajalawana41@gmail.com>
2026-04-26 14:25:32 -0700

    resolving supabase error

--- backend/src/lib/supabase.js
+++ backend/src/lib/supabase.js
@@ -4,13 +4,13 @@ dotenv.config();
 
 
 console.log("here supabaseURL:", process.env.SUPABASE_URL)
-console.log("KEY:", process.env.SUPABASE_ANON_KEY)
+console.log("KEY:", process.env.SUPABASE_SERVICE_KEY)
 
 
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_KEY
 );
 
 export default supabase;
