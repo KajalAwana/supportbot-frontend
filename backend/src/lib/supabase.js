c15a9f005b3e97327be942d2e0bc0e39769bbde2
Kajal <kajalawana41@gmail.com>
2026-04-26 14:10:22 -0700

    resolving supabase error

--- backend/src/lib/supabase.js
+++ backend/src/lib/supabase.js
@@ -10,7 +10,7 @@ console.log("KEY:", process.env.SUPABASE_ANON_KEY)
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 export default supabase;
